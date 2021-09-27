04419a2808c17ef68cc46f6c427c668777269b14
anavesilva <anavasconcelosesilva@gmail.com>
2021-09-27 14:47:34 -0300

    primeiro commit

---
+++ Desafios/desafio02.py
@@ -0,0 +1,5 @@
+dia=input('Informe o dia do seu nascimento:')
+mes=input('Informe o mês de seu nascimento:')
+ano=input('Informe o ano de seu nascimento:')
+print('Você nasceu no dia',dia,'de',mes,'de',ano,',correto?')
+
