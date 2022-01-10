54e4c551e0156ec7129b676b0d90d91475d9c8e9
anavesilva <anavasconcelosesilva@gmail.com>
2022-01-10 10:51:01 -0300

    ex055

--- exercicios-python/ex055.py
+++ exercicios-python/ex055.py
@@ -1,1 +1,15 @@
 #Exercício Python 55: Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
+maior = 0
+menor = 0
+for c in range(1, 6):
+    p = float(input('Digite o peso da {}º pessoa: '.format(c)))
+    if c == 1:
+        maior = p
+        menor = p
+    else:
+        if p > maior:
+            maior = p
+        if p < menor:
+            menor = p
+print('O maior peso foi {}Kg.'.format(maior))
+print('O menor peso foi {}Kg.'.format(menor))
