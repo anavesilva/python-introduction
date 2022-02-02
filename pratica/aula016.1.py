d85fb9381e348262584fd2942e4818ee822adbe5
anavesilva <anavasconcelosesilva@gmail.com>
2022-02-02 09:19:37 -0300

    Add prática aula 16

---
+++ pratica/aula016.1.py
@@ -0,0 +1,7 @@
+lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim', 'Batata frita')
+# cont = len(lanche)
+# print(cont)
+#Dessa forma é possível mostrar a posição
+for cont in range(0, len(lanche)):
+    print('Eu vou comer {}.'.format(lanche[cont]))
+print('Comi muito!')
---
+++ pratica/aula016.py
@@ -0,0 +1,9 @@
+lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+for pos, comida, in enumerate(lanche):
+    print('Eu vou comer {} na posição {}.'.format(comida, pos))
+print('Eu vou comer muito!')
+# print(lanche[0])
+# print(lanche[1])
+# print(lanche[2])
+# print(lanche[3])
+# print(lanche)
